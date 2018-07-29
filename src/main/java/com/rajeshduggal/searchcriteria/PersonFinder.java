1cdacdf947d211eaceafd12593210bd304290127
Rajesh Duggal <rajesh@duggalmail.com>
2018-07-29 14:37:30 -0400

    Replace PersonFinder.Criteria func interface with built-in generic func interface Predicate

--- src/main/java/com/rajeshduggal/searchcriteria/PersonFinder.java
+++ src/main/java/com/rajeshduggal/searchcriteria/PersonFinder.java
@@ -2,15 +2,15 @@ package com.rajeshduggal.searchcriteria;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class PersonFinder {
 
-    interface Criteria {
+    //interface java.util.function.Predicate<T> {
+    //    boolean test(T t);
+    //}
 
-        boolean test(Person p);
-    }
-
-    static public List<Person> findPeople(List<Person> roster, Criteria testCriteria) {
+    static public List<Person> findPeople(List<Person> roster, Predicate<Person> testCriteria) {
         List<Person> retval = new ArrayList();
         for (Person p : roster) {
             if (testCriteria.test(p)) {
